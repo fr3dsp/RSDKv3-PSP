680da71323fd7d3008768b065e5ef2719da08559
SpanishFreddy <121837347+fr3dsp@users.noreply.github.com>
2026-01-18 17:12:11 +0100

    Better EBOOT.PBP.

--- platforms/PSP.cmake
+++ platforms/PSP.cmake
@@ -80,4 +80,6 @@ target_compile_definitions(RetroEngine PRIVATE RETRO_DISABLE_LOG=1)
 target_link_libraries(RetroEngine m)
 
 create_pbp_file(TARGET RetroEngine
-    TITLE "Sonic CD")
+    TITLE "Sonic CD"
+    ICON_PATH ../psp/ICON0.png
+    BACKGROUND_PATH ../psp/PIC1.png)
